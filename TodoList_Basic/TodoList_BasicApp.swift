3f32ec79266150d66988ce396f35f2ece3e76f39
Titus <dev@tituscoleman.com>
2023-07-19 15:20:52 -0400

    Initial Commit

---
+++ TodoList_Basic/TodoList_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_BasicApp.swift
+//  TodoList_Basic
+//
+//  Created by Titus Coleman on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
